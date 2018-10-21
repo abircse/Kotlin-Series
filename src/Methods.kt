82aa9404c049e469019c0ed92d859cb500765566
abircse <abircoxsbazar@gmail.com>
2018-10-21 23:00:50 +0600

    Hello World by Kotlin

---
+++ src/Methods.kt
@@ -0,0 +1,38 @@
+fun main(args: Array<String>) {
+
+    method1()
+    var holder = dataholder()
+    holder.servercall();
+
+    holder.call("Coxtunes Software Limited")
+    holder.name
+
+    print("outPUT is  ${holder.name}")
+
+}
+
+fun method1 ()
+{
+    println("Response from method1")
+
+}
+
+///----if you create a class & put all function inside this class so lets do it---///
+class dataholder
+{
+
+
+    var name: String = "ABIR"
+
+    fun servercall ()
+    {
+        println("Response from method1")
+
+    }
+
+    fun call (param: String)
+    {
+        println(param)
+
+    }
+}
