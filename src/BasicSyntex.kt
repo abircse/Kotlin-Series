240f0cd5386d9beede5ce2d4c729dc5ab00e90e8
abircse <abircoxsbazar@gmail.com>
2018-10-21 22:26:01 +0600

    Hello World by Kotlin

---
+++ src/BasicSyntex.kt
@@ -0,0 +1,17 @@
+fun main(args: Array<String>) {
+
+    var mystring = "NayeeM Shiddiki Abir"
+    println(mystring)
+    var mynum = 10;
+    println(mynum)
+
+    var Url : String
+    Url = "http://www.coxtunes.com"
+    println(Url)
+
+    ///-----if we use val replace on var it value will not change---//
+    val abir : Int
+    abir = 100;
+    print(abir)
+
+}
